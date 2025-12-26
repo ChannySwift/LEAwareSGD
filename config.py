ee218ce831c29ac96468d8adbc367090638d9889
Ning Chen <44316347+ChannySwift@users.noreply.github.com>
2025-12-26 15:55:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,6 @@
-DomainNet_DATA_FOLDER = "/home/Cinging/Dataset/DomainNet"
-# DOMANINET_DATA_FOLDER = "/data/datasets/Cinging/DomainNet"
-
-# OfficeHome_DATA_FOLDER = "/data/datasets/Cinging/OfficeHome"
-OfficeHome_DATA_FOLDER = "/home/Cinging/Dataset/OfficeHome"
-
-VLCS_DATA_FOLDER = "/home/Cinging/Dataset/VLCS"
-
-PACS_DATA_FOLDER = "/home/Cinging/Dataset/PACS"
-# PACS_DATA_FOLDER = "/data/datasets/Cinging/PACS"   #H800
-
-
-TerraIncognita_DATA_FOLDER = "/data/datasets/Cinging/TerraIncognita" #H800
-
-# Fundus_DATA_FOLDER = "/data/datasets/Cinging/Fundus" #H800
-Fundus_DATA_FOLDER = "/home/Cinging/Dataset/Fundus" #A100
-
-# DOMANINET_DATA_FOLDER = "/root/dataset/DomainNet"
-# PACS_DATA_FOLDER = "/root/dataset/PACS"
-# OfficeHome_DATA_FOLDER = "/root/dataset/OfficeHome"
+DomainNet_DATA_FOLDER = "/root/dataset/DomainNet"
+OfficeHome_DATA_FOLDER = "/root/dataset/OfficeHome"
+VLCS_DATA_FOLDER = "/root/dataset/VLCS"
+PACS_DATA_FOLDER = "/root/dataset/Dataset/PACS"
+TerraIncognita_DATA_FOLDER = "/your_dir/dataset/TerraIncognita"
+Fundus_DATA_FOLDER = "/root/dataset/Fundus"
